73e7a74abb8c3289b6bf8c56586ae964e7002102
saadany <saadany165@gmail.com>
2025-02-10 16:00:50 +0200

    db package

---
+++ src/db.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Test\Packeg;
+
+class db
+{
+    private $table;
+    private $sql;
+    private $connection;
+
+    public function __construct($host, $user, $password, $dd,$table)
+    {
+        $this->connection = mysqli_connect($host, $user, $password, $dd);
+        $this->table=$table;
+    }
+    public function insert($data)
+    {
+        $columns="";
+        $values="";
+        foreach ($data as $column => $value) {
+            $columns.="`$column`,";
+            $values.="'$value',";
+        }
+        $columns=trim($columns,",");
+        $values=trim($values,",");
+        $this->sql="INSERT INTO `$this->table` ($columns) VALUES ($values)";
+        return $this;
+    }
+
+    public function select($column="*")
+    {
+        $this->sql="SELECT * FROM `$this->table`";
+        return $this;
+    }
+
+    public function update($data)
+    {
+        $set="";
+        foreach ($data as $column => $value) {
+            $set.="`$column`='$value',";
+        }
+        $set=trim($set,",");
+        $this->sql="UPDATE $this->table SET $set";
+        return $this;
+    }
+
+    public function delete()
+    {
+        $this->sql="DELETE FROM `$this->table`";
+        return $this;
+    }
+
+    public function excute()
+    {
+        mysqli_query($this->connection,$this->sql);
+    }
+
+    public function all()
+    {
+        echo "<pre>";
+        $result=mysqli_query($this->connection,$this->sql);
+        print_r(mysqli_fetch_all($result,MYSQLI_ASSOC));
+    }
+    public function show()
+    {
+        $result=mysqli_query($this->connection,$this->sql);
+        echo "<pre>";
+        print_r (mysqli_fetch_assoc($result));
+
+    }
+
+    public function where($column,$operator,$value)
+    {
+        $this->sql.=" WHERE `$column` $operator '$value'";
+        return $this ;
+    }
+
+
+    public function Andwhere($column,$operator,$value)
+    {
+        $this->sql.=" AND `$column` $operator '$value'";
+        return $this ;
+    }
+
+    public function Orwhere($column,$operator,$value)
+    {
+        $this->sql.=" OR `$column` $operator '$value'";
+        return $this ;
+    }
+
+
+}
